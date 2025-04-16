842b359460336a340ed503f5b26277a2eae9b07b
ananducv <ananducv666@gmail.com>
2025-04-16 14:49:50 +0530

    stack auth redirect 404 fix

--- packages/frontend-web/app/[locale]/handler/[...stack]/page.tsx
+++ packages/frontend-web/app/[locale]/handler/[...stack]/page.tsx
@@ -1,5 +1,5 @@
 import { StackHandler } from "@stackframe/stack";
-import { stackServerApp } from "../../../stack";
+import { stackServerApp } from "../../../../stack";
 
 export default function Handler(props: unknown) {
   return <StackHandler fullPage app={stackServerApp} routeProps={props} />;
