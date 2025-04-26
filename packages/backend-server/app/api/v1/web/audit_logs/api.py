0c9162a345e60639497fa3786e158b00d8ca44b5
itsmeakhil <akhiledathadan007@gmail.com>
2025-04-26 10:16:49 +0530

    walle, audit log

--- packages/backend-server/app/api/v1/web/audit_logs/api.py
+++ packages/backend-server/app/api/v1/web/audit_logs/api.py
@@ -1,27 +1,24 @@
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Request
 from app.api.v1.web.audit_logs.services import get_audit_logs, get_aduit_log_actions
 from app.framework.permission_services.service import get_current_user
 from app.api.v1.web.auth.schema import UserDetails
-
+from app.api.v1.web.audit_logs.schema import AuditLogList
 router = APIRouter()
 AUDIT_LOGS="/{workspace_id}/audit-logs"
 
 
 
-@router.get(AUDIT_LOGS)
+@router.post(AUDIT_LOGS)
 async def get_audit_logs_api(
     workspace_id: str,
-    skip: int = Query(default=0, description="Skip"),
-    limit: int = Query(default=20, description="Limit"),
+    payload: AuditLogList,
+    request: Request,
     user: UserDetails = Depends(get_current_user),
 ):  
-    query = {
-        "workspace_id": workspace_id
-    }
-    return get_audit_logs(user.get("db"), query, skip, limit)
+    return get_audit_logs(user.get("db"), payload.model_dump(), request)
 
-
-@router.get("/audit-log-actions")
+    
+@router.get("/audit-log-actions")          
 async def get_audit_log_actions_api(
     user: UserDetails = Depends(get_current_user),
 ):
