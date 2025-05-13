bb0eadb8f70730d567b2786300b5e3d258a681c9
itsmeakhil <akhiledathadan007@gmail.com>
2025-05-13 11:32:52 +0530

    remove passlib

--- packages/backend-server/app/api/v1/web/auth/utils.py
+++ packages/backend-server/app/api/v1/web/auth/utils.py
@@ -1,15 +1,5 @@
-from passlib.context import CryptContext
 from app.managers import user_keys as user_keys_manager
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-def get_password_hash(password: str):
-    return pwd_context.hash(password)
-
-
-def check_password(password: str, hashed_password: str):
-    return pwd_context.verify(password, hashed_password)
 
 
 def get_public_key(db, user_id):
