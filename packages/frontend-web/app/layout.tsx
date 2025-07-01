23913c39fa268fbefc689a6679ff110c0b7cb44c
ananducv <ananducv666@gmail.com>
2025-07-01 18:21:34 +0530

    Add Twitter conversion tracking script to layout
    Inserted Twitter conversion tracking base code into the main layout to enable analytics and conversion tracking for Twitter ads.

--- packages/frontend-web/app/layout.tsx
+++ packages/frontend-web/app/layout.tsx
@@ -24,6 +24,19 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
+        {/* Twitter conversion tracking base code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+              },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+              a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+              twq('config','q2wa1');
+            `
+          }}
+        />
+        {/* End Twitter conversion tracking base code */}
+        
         <StackProvider app={stackServerApp}>
           <StackTheme>
             <ThemeProvider
