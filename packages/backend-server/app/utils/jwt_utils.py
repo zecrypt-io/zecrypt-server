09a17bff56bf1f5bdbe4f6ad13230dc9fd448793
itsmeakhil <akhiledathadan007@gmail.com>
2025-07-10 14:03:49 +0530

    Update jwt_utils.py

--- packages/backend-server/app/utils/jwt_utils.py
+++ packages/backend-server/app/utils/jwt_utils.py
@@ -16,13 +16,13 @@ def create_jwt_token(
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(days=2)
+        expire = datetime.utcnow() + timedelta(days=30)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, jwt_secret, algorithm=jwt_algorithm)
     return encoded_jwt
 
 
-def encode_token(userid, days=30, ttl=None, device_id=None):
+def encode_token(userid, days=60, ttl=None, device_id=None):
     user_id = userid
     payload = {
         "user": str(user_id),
